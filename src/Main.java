85948d9e25d9235365ba604fc94e9f2b42225012
Asem Nurlanova <240748@astanait.edu.kz>
2024-12-27 10:20:18 +0600

    created person class and its subclasses

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,3 @@
-
 import java.io.File;
 import java.util.ArrayList;
 import java.util.Scanner;
@@ -7,7 +6,6 @@ public class Main {
     public static void main(String[] args) throws Exception {
         School school = new School();
 
-        // Add sample students
         Student student1 = new Student("Harry", "Potter", 21, true);
         student1.AddGrade(78);
         student1.AddGrade(89);
@@ -21,10 +19,8 @@ public class Main {
         student2.AddGrade(75);
         student2.AddGrade(63);
         student2.AddGrade(82);
-
         school.addMember(student2);
 
-        // Add sample teachers
         Teacher teacher1 = new Teacher("Severus", "Snape", 51, true, "Math", 7, 800000);
         teacher1.Raise(10);
         school.addMember(teacher1);
@@ -32,10 +28,8 @@ public class Main {
         Teacher teacher2 = new Teacher("Albus", "Dumbledore", 71, true, "English", 25, 1500000);
         school.addMember(teacher2);
 
-        // Print all members
         System.out.println(school);
 
-        // Display GPA of students
         for (Person member : school.getMembers()) {
             if (member instanceof Student) {
                 Student student = (Student) member;
